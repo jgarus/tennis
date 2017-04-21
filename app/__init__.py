87104a9287cd00df727838a58587ef4172f603ca
jesus <javprg7@gmail.com>
2017-04-21 12:25:14 -0400

    Added pagination

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,6 @@ from config import app_config
 # db initialization object used to interact with the database
 db = SQLAlchemy()
 
-
-
 # loads configuration from the config.py file
 # also loads the configuration from the instance/config.py file
 # imported in ~run.py
@@ -31,11 +29,11 @@ def create_app(config_name):
     from app import models
 
 
-
     # importing the home Blueprint
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
 
+    # importing the home Blueprint
     from .players import players as players_blueprint
     app.register_blueprint(players_blueprint)
 
--- app/players/views.py
+++ app/players/views.py
@@ -1,17 +1,18 @@
 # app/players/views.py
-
+from flask_paginate import Pagination
 from flask import render_template
 
 from . import players
 from ..models import Player
 
-@players.route('/players', defaults={'page':1})
+
+@players.route('/players', defaults={'page':1}, methods=["GET"])
+@players.route('/players/<int:page>', methods=["GET"])
 def playerspage(page):
-    players = Player.query.all()
-    perpage=20
-    startat=page*perpage
+    per_page=20
 
-
+    # paginated = Player.query.paginate(page,per_page, False)
+    players = Player.query.paginate(page, per_page, False)
 
     return render_template('players/players.html', players=players,
                            title="Players")
